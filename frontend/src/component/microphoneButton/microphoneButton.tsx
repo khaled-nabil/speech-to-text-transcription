335bf968a6b40b3540b6c6c7045301f0d6bedb48
Khaled Kadoura <khaled.nabil.k@gmail.com>
2025-11-14 01:44:02 +0100

    feat: implement audio recording functionality with start, pause, and stop controls

--- frontend/src/component/microphoneButton/microphoneButton.tsx
+++ frontend/src/component/microphoneButton/microphoneButton.tsx
@@ -1,20 +1,55 @@
 import { IconButton } from '@mui/material'
 import MicIcon from '@mui/icons-material/Mic'
-
+import StopIcon from '@mui/icons-material/Stop'
+import PauseIcon from '@mui/icons-material/Pause'
 import style from './microphoneButton.module.scss'
+import { useAudioRecorder } from './useAudioRecorder'
 
 interface MicrophoneButtonProps {
-	onClick: () => void
+	onRecordingComplete: (blob: Blob) => void
+	onRecording: (active: boolean) => void
 }
 
-const MicrophoneButton = ({ onClick }: MicrophoneButtonProps) => {
+const MicrophoneButton = ({
+	onRecordingComplete,
+	onRecording,
+}: MicrophoneButtonProps) => {
+	const { state, start, pause, resume, stop } =
+		useAudioRecorder(onRecordingComplete)
+
+	const handleRecordPause = () => {
+		if (state === 'INACTIVE') {
+			start()
+			onRecording(true)
+		} else if (state === 'RECORDING') {
+			pause()
+			onRecording(false)
+		} else {
+			resume()
+			onRecording(true)
+		}
+	}
+
+	const handleStop = () => {
+		stop()
+		onRecording(false)
+	}
+
 	return (
-		<IconButton
-			onClick={onClick}
-			className={style.button}
-		>
-			<MicIcon sx={{ fontSize: 40 }} />
-		</IconButton>
+		<div className={style.container}>
+			<IconButton onClick={handleRecordPause} className={style.button}>
+				{state === 'RECORDING' ? (
+					<PauseIcon sx={{ fontSize: 40 }} />
+				) : (
+					<MicIcon sx={{ fontSize: 40 }} />
+				)}
+			</IconButton>
+			{state !== 'INACTIVE' && (
+				<IconButton onClick={handleStop} className={style.button}>
+					<StopIcon sx={{ fontSize: 40 }} />
+				</IconButton>
+			)}
+		</div>
 	)
 }
 
