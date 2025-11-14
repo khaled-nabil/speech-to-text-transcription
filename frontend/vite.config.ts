2bf0e72998273f8c9c691f790ff28146bf611b05
Khaled Kadoura <khaled.nabil.k@gmail.com>
2025-11-14 15:18:45 +0100

    chore: add Vitest setup and mock for MediaRecorder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 import path from 'path'
 
 export default defineConfig(({ mode }) => {
